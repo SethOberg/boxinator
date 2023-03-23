17dab39dab27279d6c3e47f0a84c5bfcb4462875
Milovane <122837001+Milovane@users.noreply.github.com>
2023-03-23 09:09:33 +0100

    Update countryNotFoundException

--- src/main/java/com/project/boxinator/exceptions/CountryNotFoundException.java
+++ src/main/java/com/project/boxinator/exceptions/CountryNotFoundException.java
@@ -4,6 +4,8 @@ public class CountryNotFoundException extends RuntimeException {
     public CountryNotFoundException(int id) {
         super("Country does not exist with ID: " + id);
     }
-
+    public CountryNotFoundException(String name) {
+        super("Country does not exist with Name: " + name);
+}
 
 }
